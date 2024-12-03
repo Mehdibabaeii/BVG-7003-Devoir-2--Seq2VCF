e44a6c1336b2d0093e5871d5d9a11a4934687468
MA-Chiasson <marc-antoine.chiasson.1@ulaval.ca>
2024-12-03 14:35:46 -0500

    Update makeDir.sh

--- makeDir.sh
+++ makeDir.sh
@@ -1,8 +1,16 @@
-
 #!/bin/bash
 
-printf "\nMake structure directory: data, barcodes refgenome, results, logs\n"
+printf "\nCréation de la structure des répertoires : data, barcodes, refgenome, results, logs\n"
+
+# Création des dossiers principaux
 for i in data barcodes refgenome results logs
 do
-mkdir $i
+    mkdir -p $i
+done
+
+# Création des sous-dossiers dans 'results'
+printf "\nAjout des sous-dossiers dans 'results' : alignment, demultiplexed, qc_reports, trimmed, variants\n"
+for subdir in alignment demultiplexed qc_reports trimmed variants
+do
+    mkdir -p results/$subdir
 done
